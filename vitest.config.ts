99d8a75f47fe643a50ac55fc69861a7a619efbe8
Timothee Guerin <tiguerin@microsoft.com>
2024-09-18 14:27:11 -0700

    cleanup

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,11 +2,7 @@ import { defineConfig, defaultExclude } from "vitest/config";
 
 export default defineConfig({
   test: {
-    /* for example, use global to avoid globals imports (describe, test, expect): */
-    // globals: true,
     exclude: ["node_modules", "dist/test"],
-    // Uncomment and TS will stop being reloaded
-    //forceRerunTriggers: ['src/**'],
   },
   server: {
     watch: {
